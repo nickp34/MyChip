87fd654a4a8903f61aee69b584afdc931e0fb89e
Nick Pate <saleen34@gmail.com>
2018-07-29 08:57:21 -0700

    v1

---
+++ src/components/MyChip.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <v-combobox
+      v-model="selectedChips"
+      :items="availableChips"
+      label="Best UT Titles"
+      chips
+      clearable
+      prepend-icon="filter_list"
+      solo
+      multiple
+    >
+      <template slot="selection" slot-scope="data">
+        <v-chip
+          :selected="data.selected"
+          close
+          @input="remove(data.item)"
+        >
+          {{ data.item }}&nbsp;
+        </v-chip>
+      </template>
+    </v-combobox>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      selectedChips: [
+        'Unreal', 
+        'Untreal Tournament 2004',
+      ],
+      availableChips: [
+        'Unreal', 
+        'Unreal II', 
+        'Unreal Tournament', 
+        'Unreal Tournament 2003',
+        'Unreal Tournament 3', 
+        'Untreal Tournament 2004',        
+      ],
+    }),
+    methods: {
+      remove (item) {
+        this.selectedChips.splice(this.selectedChips.indexOf(item), 1);
+        this.selectedChips = [...this.selectedChips];
+      },
+    },
+  };
+</script>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  transitions,
+  VCombobox,
+  VChip,
+} from 'vuetify'
+import 'vuetify/src/stylus/app.styl'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    transitions,
+    VCombobox,
+    VChip,
+  },
+})
